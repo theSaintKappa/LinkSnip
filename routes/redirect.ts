9fadbb21ee65a2e6b33ad39fab9f16f2fddaa2de
SaintKappa <00wojtekz@gmail.com>
2023-03-05 02:31:35 +0100

    🍅 Add `raw` query param to snip redirect endpoint

--- routes/redirect.ts
+++ routes/redirect.ts
@@ -8,6 +8,8 @@ router.get('/:id', async (req, res) => {
 
         if (!linkSnip) return res.status(404).send('Not Found');
 
+        if (typeof req.query.raw !== 'undefined' && req.query.raw !== 'false') return res.status(200).send(linkSnip.toJSON());
+
         res.status(301).redirect(linkSnip.toJSON().redirectUrl);
     } catch (err) {
         console.error(err);
