408794acda067113b65bb83de587ee7c3ef7fa92
SaintKappa <00wojtekz@gmail.com>
2023-03-05 02:21:19 +0100

    🪙 Generate keys using Crypto API

--- README.md
+++ README.md
@@ -1,9 +1,14 @@
 # LinkSnip <img src="https://app.snip.gay/icon.png" height="26">
+
 ### Snip your long URLs in the snap of a finger! https://snip.gay<br><br>
+
 ## Instalation
-- Clone this repository
-- Run `npm i`
-- Create a `.env` file and add `REDIS_URI=[your redis connection URL]`
+
+-   Clone this repository
+-   Run `npm i`
+-   Create a `.env` file and add keys `REDIS_URI=[your redis connection URL]`, `APP_URL=[your public or local app url]`
+
 ## Running
-- Install nodemon `npm install -g nodemon`
-- Run `npm run dev`
+
+-   Install nodemon `npm install -g nodemon`
+-   Run `npm run dev`
--- routes/new.ts
+++ routes/new.ts
@@ -1,26 +1,26 @@
 import express from 'express';
 const router = express.Router();
+import crypto from 'crypto';
 import { snipRepository } from '../redis.js';
 
-const randomKey = (n) => {
-    const chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
-    let key = '';
-    for (let i = 0; i < n; i++) {
-        key += chars[Math.floor(Math.random() * chars.length)];
-    }
-    return key;
+const CHARACTERS: string = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+const randomKey = (length: number) => {
+    const randomBytes = crypto.randomBytes(length);
+
+    return Array.from(randomBytes, (byte) => CHARACTERS[byte % CHARACTERS.length]).join('');
 };
 
 router.post('/', async (req, res) => {
     try {
         if (!/[(http(s)?):\/\/(www\.)?a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/.test(req.body.url) || /(?:https?:\/\/)?snip\.gay\/?.*/.test(req.body.url)) return res.status(400).json({ code: 400, message: 'Invalid URL' });
         const url = !/^https?:\/\//i.test(req.body.url) ? `https://${req.body.url}` : req.body.url;
-        const snipId: string = randomKey(3);
+        const snipId = randomKey(3);
 
         const snipExists = await snipRepository.search().where('redirectUrl').equals(url).return.first();
         if (snipExists) return res.status(200).json({ ...snipExists.toJSON(), alreadyExisted: true });
 
-        const newSnip = await snipRepository.createAndSave({ snipId, snipUrl: `https://snip.gay/${snipId}`, redirectUrl: url, createdAt: new Date() });
+        const newSnip = await snipRepository.createAndSave({ snipId, snipUrl: `${process.env.APP_URL}/${snipId}`, redirectUrl: url, createdAt: new Date() });
         res.status(200).send({ ...newSnip.toJSON(), alreadyExisted: false });
     } catch (err) {
         console.error(err);
