be95079b2cb9e61152ce2a2fe0809c6bb6015e1d
SaintKappa <00wojtekz@gmail.com>
2023-03-06 18:25:02 +0100

    🚫 404 redirect

--- routes/redirect.ts
+++ routes/redirect.ts
@@ -6,7 +6,7 @@ router.get('/:id', async (req, res) => {
     try {
         const linkSnip = await snipRepository.search().where('snipId').equals(req.params.id).return.first();
 
-        if (!linkSnip) return res.status(404).send('Not Found');
+        if (!linkSnip) return res.status(301).redirect(`https://app.snip.gay/notfound`);
 
         if (typeof req.query.raw !== 'undefined' && req.query.raw !== 'false') return res.status(200).send(linkSnip.toJSON());
 
@@ -17,13 +17,6 @@ router.get('/:id', async (req, res) => {
     }
 });
 
-router.get('/', async (req, res) => {
-    try {
-        res.status(301).redirect('https://app.snip.gay');
-    } catch (err) {
-        console.error(err);
-        return res.status(400).send('Bad Request');
-    }
-});
+router.get('/', async (req, res) => res.status(301).redirect('https://app.snip.gay'));
 
 export default router;
